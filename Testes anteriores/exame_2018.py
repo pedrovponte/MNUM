2d6d716d354cca18860328a57d0f8fa967ad9e7c
Pedro <up201809694@fe.up.pt>
2020-01-13 00:32:02 +0000

    Update 2018 exam

--- Testes anteriores/exame_2018.py
+++ Testes anteriores/exame_2018.py
@@ -34,19 +34,42 @@ import math
 
 #ex4 - euler 2 ordem
 
-def dy(x,y,z):
-    return z
+#def dy(x,y,z):
+#    return z
+#
+#def dz(x,y,z):
+#    return -7*z - 4*y
+#
+#def euler(x,y,z,h):
+#    print("Iteration 1: ",x,y,z)
+#    for i in range(2,5,1):
+#        x0 = x
+#        x += h
+#        y += dy(x0,y,z) * h
+#        z += dz(x0,y,z) * h
+#        print("Iteration ",i,": ",x,y,z)
+#        
+#euler(0.4,2,1,0.2)
 
-def dz(x,y,z):
-    return -7*z - 4*y
+#ex5 - regra aurea
 
-def euler(x,y,z,h):
-    print("Iteration 1: ",x,y,z)
-    for i in range(2,5,1):
-        x0 = x
-        x += h
-        y += dy(x0,y,z) * h
-        z += dz(x0,y,z) * h
-        print("Iteration ",i,": ",x,y,z)
-        
-euler(0.4,2,1,0.2)  
+def f(x):
+    return (x - 4)**2 + x**4
+
+def aurea(x1,x2):
+    result = 0
+    b = (math.sqrt(5)-1)/2
+    a = b*b
+    while(x2 - x1 > 0.001):
+        x3 = x1 + a*(x2-x1)
+        x4 = x1 + b*(x2-x1)
+        if(f(x3) < f(x4)):
+            x2 = x4
+            x1 = x1
+        else:
+            x1 = x3
+            x2 = x2
+        result = (x1 + x2)/2
+    return result
+
+print(aurea(1,3))
